fd18d56614a0c489e2afe9af0a44b777946cc11f
dkarandana <dhananjayakarandana@gmail.com>
2018-06-28 17:33:01 +0530

    Tutorails and Readme configs

---
+++ src/js/modules/core/index.js
@@ -0,0 +1,11 @@
+/**
+ * @namespace core
+ * @description The parent namespace for the following modules:
+ * - {@link Storage} Storing [localstorage](@link https://www.w3.org/TR/webstorage/#the-localstorage-attribute)  data under the core.storage namespace.
+ */
+
+import storage from './storage';
+
+export default {
+    storage
+};
---
+++ src/js/modules/core/storage.js
@@ -0,0 +1,10 @@
+/**
+ * XHR Data fetching service under the core.fetchdata namespace.
+ * @requires core.storage
+ * @see core/storage
+ */
+
+
+class Storage {
+
+}
